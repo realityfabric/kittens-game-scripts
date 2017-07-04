42d18f5c8bb5cc8273ca2c7b29c08105261eabbe
realityfabric <realityfabric@gmail.com>
2017-07-04 08:47:12 -0700

    Convert tabs to spaces and strip trailing whitespace

--- kittensCore.js
+++ kittensCore.js
@@ -7,18 +7,18 @@ var linkClick = function (buttonText) {
 }
 
 var pray = function () {
-	if (gamePage.isPaused) { return; }
+    if (gamePage.isPaused) { return; }
     var faith = gamePage.resPool.get('faith');
 
     if (faith.value / faith.maxValue > 0.95) {
         linkClick("Praise the sun!");
-	}
+    }
 }
 
 var craft = function() { //https://www.reddit.com/r/kittensgame/comments/2eqlt5/a_few_kittens_game_scripts_ive_put_together/
-	if (gamePage.isPaused) { return; }
+    if (gamePage.isPaused) { return; }
 
-	if (debug_on) console.log ("craft()");
+    if (debug_on) console.log ("craft()");
 
     var resources = [
         ["catnip",   "wood"],
@@ -30,24 +30,24 @@ var craft = function() { //https://www.reddit.com/r/kittensgame/comments/2eqlt5/
     ];
 
     var affordable = function (craft) {
-		if (debug_on) console.log ("Checking affordability of " + craft);
+        if (debug_on) console.log ("Checking affordability of " + craft);
 
-		var cost = gamePage.workshop.getCraft(craft).prices;
-		var flag = true;
+        var cost = gamePage.workshop.getCraft(craft).prices;
+        var flag = true;
 
-		for (var i = 0; i < cost.length; i++) {
-			var resource = gamePage.resPool.get(cost[i].name);
-			if ((resource.maxValue != 0
-			&& cost[i].val > resource.maxValue)
-			|| cost[i].val > resource.value) {
-				flag = false;
+        for (var i = 0; i < cost.length; i++) {
+            var resource = gamePage.resPool.get(cost[i].name);
+            if ((resource.maxValue != 0
+            && cost[i].val > resource.maxValue)
+            || cost[i].val > resource.value) {
+                flag = false;
 
-				if (debug_on) console.log ("Craft is not affordable due to " + cost[i].name);
-			}
-		}
+                if (debug_on) console.log ("Craft is not affordable due to " + cost[i].name);
+            }
+        }
 
-		return flag;
-	}
+        return flag;
+    }
 
     for (var i = 0; i < resources.length; i++) {
         var curRes = gamePage.resPool.get(resources[i][0]);
@@ -58,47 +58,47 @@ var craft = function() { //https://www.reddit.com/r/kittensgame/comments/2eqlt5/
 
             if (debug_on) console.log ("Crafting 1 " + resources[i][1]);
         } else {
-			if (debug_on) console.log ("Could not craft " + resources[i][1]);
-		}
+            if (debug_on) console.log ("Could not craft " + resources[i][1]);
+        }
     }
 }
 
 
 var hunt = function() { //https://www.reddit.com/r/kittensgame/comments/2eqlt5/a_few_kittens_game_scripts_ive_put_together/
-	if (gamePage.isPaused) { return; }
+    if (gamePage.isPaused) { return; }
     var catpower = gamePage.resPool.get('manpower');
 
     if (catpower.value / catpower.maxValue > 0.95) {
         linkClick("Send hunters");
-	}
+    }
 }
 
 var books = function () { //https://www.reddit.com/r/kittensgame/comments/2eqlt5/a_few_kittens_game_scripts_ive_put_together/
-	if (gamePage.isPaused) { return; }
-	var catpower = gamePage.resPool.get('manpower');
-	var culture = gamePage.resPool.get('culture');
-	var science = gamePage.resPool.get('science');
-	var furs = gamePage.resPool.get('furs');
-	var parchment = gamePage.resPool.get('parchment');
-	var manuscript = gamePage.resPool.get('manuscript');
-	var compendium = gamePage.resPool.get('compedium');
-	var blueprint = gamePage.resPool.get('blueprint');
-	if (gamePage.workshop.getCraft('parchment').unlocked){
-		gamePage.craftAll('parchment');
-	}
-	if (gamePage.workshop.getCraft('manuscript').unlocked) {
-		if (parchment.value > 10025 && culture.value > (culture.maxValue * .9)) {
-			gamePage.craft('manuscript', 1);
-		} // gamePage.craftAll('manuscript');
-	}
-	if (gamePage.workshop.getCraft('compedium').unlocked) {
-		if (manuscript.value > 5050 && science.value > (science.maxValue * .9)) {
-			gamePage.craft('compedium', 1);
-		} // gamePage.craftAll('compedium');
-	}
-	if (gamePage.workshop.getCraft('blueprint').unlocked) {
-		if (compendium.value > 2575 && science.value > (science.maxValue * .9))  {
-			gamePage.craft('blueprint', 1);
-		}// gamePage.craftAll('blueprint');
-	}
+    if (gamePage.isPaused) { return; }
+    var catpower = gamePage.resPool.get('manpower');
+    var culture = gamePage.resPool.get('culture');
+    var science = gamePage.resPool.get('science');
+    var furs = gamePage.resPool.get('furs');
+    var parchment = gamePage.resPool.get('parchment');
+    var manuscript = gamePage.resPool.get('manuscript');
+    var compendium = gamePage.resPool.get('compedium');
+    var blueprint = gamePage.resPool.get('blueprint');
+    if (gamePage.workshop.getCraft('parchment').unlocked){
+        gamePage.craftAll('parchment');
+    }
+    if (gamePage.workshop.getCraft('manuscript').unlocked) {
+        if (parchment.value > 10025 && culture.value > (culture.maxValue * .9)) {
+            gamePage.craft('manuscript', 1);
+        } // gamePage.craftAll('manuscript');
+    }
+    if (gamePage.workshop.getCraft('compedium').unlocked) {
+        if (manuscript.value > 5050 && science.value > (science.maxValue * .9)) {
+            gamePage.craft('compedium', 1);
+        } // gamePage.craftAll('compedium');
+    }
+    if (gamePage.workshop.getCraft('blueprint').unlocked) {
+        if (compendium.value > 2575 && science.value > (science.maxValue * .9))  {
+            gamePage.craft('blueprint', 1);
+        }// gamePage.craftAll('blueprint');
+    }
 }
